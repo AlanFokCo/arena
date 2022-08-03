1576c2f203c0362238a9ab8fa40fd5bf57c216e5
AlanFokCo <892249240@qq.com>
2022-08-03 11:42:12 +0800

    add pythonsdk pypi package

--- sdk/arena-python-sdk/setup.py
+++ sdk/arena-python-sdk/setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
-setup(name='arenasdk',
-	version='unknown',
+setup(
+	name='arenasdk',
+	version='0.10.0',
 	description='Python SDK For Arena',
-	author='happy2048',
-	author_email='2270020588@qq.com',
+	author='AlanFok',
+	url='https://arena-docs.readthedocs.io/en/latest',
+	author_email='huozhixin2868@163.com',
 	packages=find_packages(),
-	install_requires=["pyyaml","coloredlogs"]
+	install_requires=["pyyaml", "coloredlogs"]
 )
